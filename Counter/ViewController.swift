182ed7aa84e73598ec1715faca5aeda83affc72a
Iluxa147 <pokolewIlja@gmail.com>
2026-01-02 21:42:56 +0300

    initial, project created

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Ilya Pokolev on 02.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
